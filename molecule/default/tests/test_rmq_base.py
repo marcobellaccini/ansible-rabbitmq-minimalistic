2413595f99e83b339cc11571981fb15acf7a68e9
marcobellaccini <24960638+marcobellaccini@users.noreply.github.com>
2019-02-28 11:19:46 +0100

    Fixed erlang cookie test

--- molecule/default/tests/test_rmq_base.py
+++ molecule/default/tests/test_rmq_base.py
@@ -10,9 +10,3 @@ def test_rmq_conf_file(host):
     conf_file = host.file('/etc/rabbitmq/rabbitmq.conf')
 
     assert conf_file.exists
-
-
-def test_rmq_cookie_file(host):
-    cookie_file = host.file('/var/lib/rabbitmq/.erlang.cookie')
-
-    assert cookie_file.contains('XXXXXXXXXXXXXXXXXXXX')
